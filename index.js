32b06487aaa2f62362474d370bc238d1fb6aa92a
surveshailesh02 <surveshailesh02@gmail.com>
2024-07-12 12:24:50 +0530

    Project Initialization
    Project Initialization

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("This is starting point");
+
+console.log("=============== Global Objects=============");
+console.log(console);
+console.log(global);
+console.log(process);
+console.log(__filename);
+console.log(__dirname);
+console.log(module);
